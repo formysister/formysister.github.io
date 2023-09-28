42c43dbc23045feab52d4a3dd10b30163d4a0ceb
formysister <enemymaycry2007@gmail.com>
2023-09-28 20:12:38 +0900

    repository initialized

---
+++ precache-manifest.e7d3adab41a74fb570ff5c4d562d20fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "410eca9751df43c35372",
+    "url": "/static/css/main.723eaaf8.chunk.css"
+  },
+  {
+    "revision": "410eca9751df43c35372",
+    "url": "/static/js/main.410eca97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ed934deeb788c0cb18e3",
+    "url": "/static/js/2.ed934dee.chunk.js"
+  },
+  {
+    "revision": "2fbbd567a0b98b4e8d569dd1b3323c09",
+    "url": "/static/media/cv.2fbbd567.svg"
+  },
+  {
+    "revision": "a326448d912f62e41f1d95e60793ec1d",
+    "url": "/static/media/ilyahacker.a326448d.svg"
+  },
+  {
+    "revision": "5a8a44becefc324713acd912feefc88d",
+    "url": "/static/media/song.5a8a44be.mp3"
+  },
+  {
+    "revision": "a76c4df5d6b9641550cdb64c25a218a7",
+    "url": "/index.html"
+  }
+];
